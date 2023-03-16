f9a500464bec8d316592b2d1720b01afd033527a
kenterberiiskii <a.mizyakov@gmail.com>
2023-03-16 17:39:00 +0300

    les3

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models.database import db
+from blog.views.auth import login_manager, auth_app
 
 app = Flask(__name__)
 
@@ -12,3 +14,11 @@ def index():
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.register_blueprint(auth_app, url_prefix="/auth")
+
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+login_manager.init_app(app)
